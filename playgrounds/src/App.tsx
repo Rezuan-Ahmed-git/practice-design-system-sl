9dbb9f9eee4c3e2a85f997cec5495b1a42fa3f5d
Rezuan Ahmed <rezuan.ahmed.pro@gmail.com>
2023-01-08 18:58:12 +0600

    feat: create new atoms and foundation packages

--- playgrounds/src/App.tsx
+++ playgrounds/src/App.tsx
@@ -1,11 +1,19 @@
-import { Button } from '@ds.e/react/lib';
-
+import { Button, Color, Text } from '@ds.e/react/lib';
 
 const App = () => {
-  return <div>
-    <h1>Hello MonoRepo</h1>
-    <Button title='Alhamdulillah, I am created' onClick={() => alert("Hello World")} >Click Me</Button>
-  </div>
-}
+  return (
+    <div>
+      <h1>Hello MonoRepo</h1>
+      <Text size="xl">Hellow World</Text>
+      <Color hexCode="#f1f1f1" width="xxxl" height="xxxl" />
+      <Button
+        title="Alhamdulillah, I am created"
+        onClick={() => alert('Hello World')}
+      >
+        Click Me
+      </Button>
+    </div>
+  );
+};
 
 export default App;
