4338392bc405cb2aff3a6112c1e09769d23d2751
Rezuan Ahmed <rezuan.ahmed.pro@gmail.com>
2023-01-08 08:49:50 +0600

    feat: setup mono repo with two packages

---
+++ playgrounds/src/App.tsx
@@ -0,0 +1,11 @@
+import { Button } from '@ds.e/react/lib';
+
+
+const App = () => {
+  return <div>
+    <h1>Hello MonoRepo</h1>
+    <Button title='Alhamdulillah, I am created' onClick={() => alert("Hello World")} >Click Me</Button>
+  </div>
+}
+
+export default App;
